d646d94888ea164a67f2627ec21c8c5ab426d11e
Fred Lindsey <fred.lindsey.92@gmail.com>
2022-06-15 17:16:36 -0500

    finished wrangle, began univariate stats explore

--- zillow_clustering_project/wrangle.py
+++ zillow_clustering_project/wrangle.py
@@ -40,21 +40,6 @@ from sklearn.model_selection import train_test_split
 def get_connection(db, user=env.user, host=env.host, password=env.password):
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
 
-#____________________________________________________________________________________
-
-def get_mall_customers(use_cache=True):
-    """Retrieves mall Customer dataset from CodeUp server"""
-    filename = "mall_customers.csv"
-    if os.path.isfile(filename) and use_cache:
-        return pd.read_csv(filename)
-    else:
-        df = pd.read_sql("""
-        SELECT * 
-        FROM customers;
-        """
-        , get_connection('mall_customers'))
-        df.to_csv(filename, index=False)
-        return df
 
 #____________________________________________________________________________________
 
@@ -85,17 +70,39 @@ def get_zillow_data(use_cache=True):
         , get_connection('zillow'))
         df.to_csv(filename, index=False)
         return df
-#__________________________________________________________________________________
-# outlier handling to remove quant_cols with >3.5 z-score (std dev)
 
-def remove_outliers(threshold, quant_cols, df):
-    z = np.abs((stats.zscore(df[quant_cols])))
-    df=df[(z < threshold).all(axis=1)]
-    # remove homes with 0 BR/BD or SQ FT from the final df
-    df = df[(df.bedroomcnt != 0) & (df.bathroomcnt != 0) & 
-    (df.calculatedfinishedsquarefeet >= 69)]
+#_______________________________________________________________________________________
+# handles missing values, drops columns that are 30% empty, drops rows that are 25% empty
+# drops duplicate columns as well
+def handle_missing_values(df, prop_required_column = .7, prop_required_row=.75):
+    threshold = int(round(prop_required_column*len(df.index), 0))
+    df.dropna(axis=1, thresh=threshold, inplace=True)
+    threshold = int(round(prop_required_row*len(df.columns), 0))
+    df.dropna(axis=0, thresh=threshold, inplace=True)
+    df = df.drop_duplicates(keep='first')
     return df
 
+#_______________________________________________________________________________________
+# handles missing values, drops columns that are 30% empty, drops rows that are 25% empty
+# drops duplicate columns as well
+def handle_remaining_nulls(df):
+    null_columns = ['calculatedbathnbr','fullbathcnt', 'lotsizesquarefeet', 'yearbuilt', 
+    'structuretaxvaluedollarcnt', 'calculatedfinishedsquarefeet','taxvaluedollarcnt', 'landtaxvaluedollarcnt',
+    'taxamount']
+    for col in null_columns:
+        df[col] = df[col].fillna(df[col].mean())
+    return df
+
+#__________________________________________________________________________________
+# these columns needed to be removed because they added no useful information, and 
+# regionidzip caused an issue in data wrangling
+
+def remove_columns(df):
+    cols_to_remove = ['propertylandusetypeid', 'id', 'id.1', 'finishedsquarefeet12', 
+    'regionidzip', 'rawcensustractandblock', 'assessmentyear', 'censustractandblock',
+    'regionidcity']
+    df = df.drop(columns=cols_to_remove)
+    return df
 #__________________________________________________________________________________
 def map_counties(df):
     # identified counties for fips codes 
@@ -106,25 +113,41 @@ def map_counties(df):
     df.fips = df.fips.map(counties)
     return df
 #__________________________________________________________________________________
-cols_to_remove = ['heatingorsystemtypeid', 'propertylandusetypeid', 'id', 'id.1',
-'buildingqualitytypeid']
-
-def remove_columns(df, cols_to_remove):
-    df = df.drop(columns=cols_to_remove)
+def convert_data_types(df):
+    # identified columns to change to object types, for later encoding
+    non_quant_cols = ['parcelid', 'regionidcounty', 'fips', 'latitude', 'longitude']
+    for col in non_quant_cols:
+        df[col] = df[col].astype('object')
     return df
 #__________________________________________________________________________________
-# handles missing values, drops columns that are 50% empty, drops rows that are 25% empty
-# drops duplicate columns as well
-def handle_missing_values(df, prop_required_column = .5, prop_required_row=0.75):
-    threshold = int(round(prop_required_column*len(df.index), 0))
-    df.dropna(axis=1, thresh=threshold, inplace=True)
-    threshold = int(round(prop_required_row*len(df.columns), 0))
-    df.dropna(axis=0, thresh=threshold, inplace=True)
-    df = df.drop_duplicates(keep='first')
+# outlier handling to remove quant_cols with >3 z-score (std dev) for float columns 
+# other than the target variable. threshold is the level of zscore or standard deviations
+# from the mean, which the function will use as a ceiling
+# eliminte nulls first
+
+def remove_outliers(df, threshold=3):
+     # set quant_cols 
+    quant_cols = []
+    for col in df.columns:
+        if df[col].dtype == 'float64' and col != 'logerror':
+            quant_cols.append(col)
+    print(quant_cols)
+    # remove df rows where z_score > 3.5 for quant_cols
+    z = np.abs((stats.zscore(df[quant_cols])))
+    print(z)
+    df = df[(z < threshold).all(axis=1)]
+    print(df.head(20))
+    # remove homes with 0 BR/BD or SQ FT from the final df
+    df = df[(df.bedroomcnt != 0) & (df.bathroomcnt != 0) & 
+    (df.calculatedfinishedsquarefeet >= 69)]
     return df
+
+
 #__________________________________________________________________________________
 
-# def get_upper_outliers(s, k):
+#__________________________________________________________________________________
+
+# def q1_logerror(s, k):
 #     '''
 #     Given a series and a cutoff value, k, returns the upper outliers for the
 #     series.
@@ -139,7 +162,7 @@ def handle_missing_values(df, prop_required_column = .5, prop_required_row=0.75)
 
 #__________________________________________________________________________________
 
-# def get_lower_outliers(s, k):
+# def q4_logerror(s, k):
 #     '''
 #     Given a series and a cutoff value, k, returns the lower outliers for the
 #     series.
@@ -153,37 +176,44 @@ def handle_missing_values(df, prop_required_column = .5, prop_required_row=0.75)
 #     return s.apply(lambda x: min([x - lower_bound, 0]))
 
 #__________________________________________________________________________________
-# def add_outlier_columns(df, k):
-#     '''
-#     Add a column with the suffix _outliers for all the numeric columns
-#     in the given dataframe.
-#     '''
-#     # outlier_cols = {col + '_outliers': get_upper_outliers(df[col], k)
-#     #                 for col in df.select_dtypes('number')}
-#     # return df.assign(**outlier_cols)
-
-#     for col in df.select_dtypes('number'):
-#         df_outliers = df[col + '_outliers'] = get_upper_outliers(df[col], k)
-
-#     return df_outliers
-
-#__________________________________________________________________________________
-def data_prep(df):
+def prep_zillow(df):
     """
     Takes in zillow Dataframe from the get_zillow_data function.
-    Arguments: drops unnecessary columns, removes null columns and rows above
+    Arguments: 
     threshold, maps county names to FIPS, and removes outliers > 3.5 z score above
     mean.
     Returns cleaned data.
     """
     
     df = handle_missing_values(df)
-    df = remove_columns(df, cols_to_remove)
+    df = handle_remaining_nulls(df)
+    df = remove_columns(df)
     df = map_counties(df)
-    #df_outliers = add_outlier_columns(df, k=1.5)
-    return df #df_outliers
+    df = convert_data_types(df)
+    df = remove_outliers(df, threshold=3)
+    return df 
+#__________________________________________________________________________________
 
-
+def wrangle_zillow():
+    df = get_zillow_data()
+    df = prep_zillow(df)
+    return df
+#__________________________________________________________________________________
+def encode_zillow(df):
+    """
+    Takes in Zillow Dataframe.
+    Encodes object type columns.    
+    Returns encoded_df.
+    """
+    #Get dummies for non-binary categorical variables:
+    dummies_list = df.select_dtypes('object').columns
+    dummy_df = pd.get_dummies(df[dummies_list], dummy_na = False, drop_first=True)
+    #concatenate the two dataframes
+    df = pd.concat([df, dummy_df], axis=1)
+    #rename the encoded df
+    encoded_df = df.drop(columns=dummies_list)
+    #return the encoded df
+    return encoded_df
 #________________________________________________________
 #scaler = sklearn.preprocessing.MinMaxScaler()
 #scaler = sklearn.preprocessing.StandardScaler()
@@ -202,76 +232,7 @@ def fit_and_scale(scaler, train, validate, test):
     scaled_test = pd.DataFrame(data=scaler.transform(test[floats]), columns=floats)
     return scaled_train, scaled_validate, scaled_test
 
-#__________________________________________________________________________________
-def encode_zillow(df):
-    """
-    Takes in Zillow Dataframe.
-    Encodes object type columns.    
-    Returns encoded_df.
-    """
-    #Get dummies for non-binary categorical variables:
-    dummies_list = df.select_dtypes('object').columns
-    dummy_df = pd.get_dummies(df[dummies_list], dummy_na = False, drop_first=True)
-    #concatenate the two dataframes
-    df = pd.concat([df, dummy_df], axis=1)
-    #rename the encoded df
-    encoded_df = df.drop(columns=dummies_list)
-    #return the encoded df
-    return encoded_df
-#__________________________________________________________________________________
 
-def wrangle_zillow():
-    df = get_zillow_data()
-    df = data_prep(df)
-    #df = get_upper_outliers(df, k=1.5)
-    return df
 
-#__________________________________________________________________________________
 
-# pred_2017.parcelid AS ID, bedroomcnt, bathroomcnt,
-# 		calculatedfinishedsquarefeet AS sq_ft,
-#         lotsizesquarefeet AS lot_size,
-#         taxvaluedollarcnt AS home_price, 
-#         yearbuilt, taxamount, fips AS county, latitude, longitude,
-#         pred_2017.logerror AS log_error
 
-#__________________________________________________________________________________
-# Original prep zillow function
-
-# def prep_zillow(df):
-#     """
-#     Takes in zillow Dataframe from the get_zillow_data function.
-#     Arguments: drops unnecessary columns, 0 value columns, duplicates,
-#     and converts select columns from float to int.
-#     Returns cleaned data.
-#     """
-#     # remove empty entries stored as whitespace, convert to nulls
-#     print('before replace whitespace')
-#     df = df.replace(r'^\s*$', np.nan, regex=True)
-#     print('after replace whitespace')
-#     # drop columns with more than 30% null values
-#     print('before drop 30% null rows')
-#     df = df.dropna(axis=1, thresh=(3*(len(df)/10)))
-#     print('before drop 30% null rows')
-#     # drop any duplicate rows
-#     df = df.drop_duplicates(keep='first')
-#     print('before non-quants')
-#     # convert column types from float to int
-#     non_quants = ['fips', 'parcelid', 'id', 'latitude', 'longitude', 
-#     'regionidcity', 'propertylandusetypeid', 'propertyzoningdesc', 'transactiondate',
-#      'propertylandusedesc']
-#     df[non_quants] = df[non_quants].astype('object')
-#     print('after quants')
-#     # remove homes with 0 BR/BD or SQ FT from the final df
-#     df = df[(df.bedroomcnt != 0) & (df.bathroomcnt != 0) & 
-#     (df.calculatedfinishedsquarefeet >= 69)]
-#     # remove all rows where any column has z score gtr than 3
-#     quants = df.drop(columns=non_quants).columns
-#     #quants = df.astype()
-#     # outlier handling
-#     # remove numeric values with > 3.5 std dev
-#     df = remove_outliers(3.5, quants, df)
-#     df = map_counties(df)
-
-#     return df
-#__________________________
